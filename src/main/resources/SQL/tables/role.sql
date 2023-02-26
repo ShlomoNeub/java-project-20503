6fb7c92206c8ff4720604ea4c233279881315bb3
Shlomo Neuberger <Shlomo@xoltar.com>
2023-02-26 20:16:48 +0200

    Set columns to be not null as defined in architecture

--- src/main/resources/SQL/tables/role.sql
+++ src/main/resources/SQL/tables/role.sql
@@ -3,7 +3,7 @@ create table "Roles"
     role_id    integer generated always as identity
         constraint "Roles_pk"
             primary key,
-    role_name  varchar,
-    role_level integer
+    role_name  varchar not null ,
+    role_level integer not null
 );
 
