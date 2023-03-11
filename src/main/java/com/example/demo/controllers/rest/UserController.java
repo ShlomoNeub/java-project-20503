088999df36e79739363487725544a2e5787b01f3
Shlomo Neuberger <Shlomo@xoltar.com>
2023-03-12 00:43:28 +0200

    Finish user RS implementation

--- src/main/java/com/example/demo/controllers/rest/UserController.java
+++ src/main/java/com/example/demo/controllers/rest/UserController.java
@@ -5,12 +5,13 @@ import com.example.demo.db.repo.UserRepo;
 import org.springframework.web.bind.annotation.*;
 import org.apache.logging.log4j.*;
 
-
-
+/**
+ * Controller that implements the Users REST API
+ */
 @RestController
 @RequestMapping(path = "/users")
-public class UserController extends RestApiAbstract<Users,UserRepo> {
-    Logger logger = LogManager.getLogger(UserController.class);
+public class UserController extends RestApiAbstract<Users,UserRepo,Integer> {
+    final Logger logger = LogManager.getLogger(UserController.class);
     final UserRepo repo;
 
     public UserController(UserRepo repo) {
@@ -25,20 +26,6 @@ public class UserController extends RestApiAbstract<Users,UserRepo> {
     @Override
     public Logger getLogger() {
         return logger;
-    }
-
-
-    private boolean validatePassword(String password){
-        boolean retVal = true;
-        retVal &= password.length() > Users.PASSWORD_MIN_LENGTH;
-        retVal &= password.length() <  Users.PASSWORD_MAX_LENGTH;
-        return retVal;
-    }
-
-    @Override
-    public boolean isValid(Users user) {
-        boolean isValidPassword =validatePassword(user.getPassword());
-        return isValidPassword;
     }
 
 }
--- src/main/java/com/example/demo/db/Validatable.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.db;
-
-public interface Validatable<T> {
-
-    public boolean isValid(T t);
-
-}
