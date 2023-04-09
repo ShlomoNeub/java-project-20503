d089ef83cba05479b5d18715bd5ef66bb0e0e751
Yossi Shemer <26yossi262@gmail.com>
2023-04-09 14:08:32 +0300

    different controllers for classes Constraint ConstraintType AvailableShifts with implemented comments

--- src/main/java/com/example/demo/controllers/rest/AvailableShiftsController.java
+++ src/main/java/com/example/demo/controllers/rest/AvailableShiftsController.java
@@ -1,59 +1,71 @@
 package com.example.demo.controllers.rest;
 
-
 import com.example.demo.db.entities.AvailableShifts;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.db.repo.AvailableShiftsRepo;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Optional;
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+/**
+ * Controller that implements the AvailableShifts REST API
+ */
 @RestController
-@RequestMapping(path = "/availableShifts")
-public class AvailableShiftsController extends RestApiAbstract<AvailableShifts, AvailableShiftsRepo,Integer>{
+@RequestMapping(path = "/available-shifts")
+public class AvailableShiftsController extends RestApiAbstract<AvailableShifts, AvailableShiftsRepo, Integer> {
+    final Logger logger = LogManager.getLogger(AvailableShiftsController.class);
+    final AvailableShiftsRepo repo;
 
-    final  AvailableShiftsRepo repo;
-    final Logger logger = Logger.getLogger(AvailableShiftsController.class .getName());
-
-    @RequestMapping(path = "/{id}/availableShifts",method = RequestMethod.GET)
-    public String getConstraints(@PathVariable Integer id){
-        Optional<AvailableShifts> availableShifts = repo.findById(id);
-        if(availableShifts.isEmpty()){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"NO ID IS FOUND"+id);
-        }
-        return availableShifts.toString();
+    public AvailableShiftsController(AvailableShiftsRepo repo) {
+        this.repo = repo;
     }
-
 
     @Override
     public AvailableShiftsRepo getRepo() {
         return repo;
     }
-    public AvailableShiftsController(AvailableShiftsRepo availableShiftsRepo) {
-        this.repo = availableShiftsRepo;
-    }
-    /*
-@RequestMapping(path = "/{id}/availableShifts",method = RequestMethod.GET)
-    public Collection<AvailableShifts> getAvailableShifts(@PathVariable Integer id) {
-        Optional<AvailableShifts> availableShifts = repo.findById(id);
-        if (availableShifts.isPresent()) {
-            return availableShifts.get().getAvailableShifts();
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "AvailableShifts not found");
-        }
+
+    @Override
+    public Logger getLogger() {
+        return logger;
     }
 
-*/
+    /**
+     * Find all available shifts for a specific week number
+     * @param weekNumber the week number to search for
+     * @return a list of available shifts for the given week number
+     */
+    @GetMapping("/week/{weekNumber}")
+    public List<AvailableShifts> findByWeekNumber(@PathVariable Integer weekNumber) {
+        return repo.findByWeekNumber(weekNumber);
+    }
 
+    /**
+     * Find shifts between days in a given week
+     * @param weekNumber the week number to search for
+     * @param startDay the start day number to search for
+     * @param endDay the end day number to search for
+     * @return a list of available shifts between the given start and end days in the specified week
+     */
+    @GetMapping("/week/{weekNumber}/days/{startDay}/{endDay}")
+    public List<AvailableShifts> findShiftsBetweenDaysInWeek(@PathVariable Integer weekNumber,
+                                                             @PathVariable Integer startDay,
+                                                             @PathVariable Integer endDay) {
+        return repo.findShiftsBetweenDaysInWeek(weekNumber, startDay, endDay);
+    }
 
-
+    /**
+     * Find the shifts with the longest duration for a given day and week
+     * @param weekNumber the week number to search for
+     * @param dayNumber the day number to search for
+     * @return a list of available shifts with the longest duration for the given day and week
+     */
+    @GetMapping("/week/{weekNumber}/day/{dayNumber}/longest-duration")
+    public List<AvailableShifts> findShiftWithLongestDuration(@PathVariable Integer weekNumber,
+                                                              @PathVariable Integer dayNumber) {
+        return repo.findShiftWithLongestDuration(weekNumber, dayNumber);
+    }
 }
