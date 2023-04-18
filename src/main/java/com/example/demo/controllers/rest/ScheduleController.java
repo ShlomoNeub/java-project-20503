bc6e44a54b2b68c457c37fa1bc8cbcd9dc3d96c2
Tal Barda <talbarda18@gmail.com>
2023-04-19 02:10:38 +0300

    API calls for worker scheduling -- finished and working!

--- src/main/java/com/example/demo/controllers/rest/ScheduleController.java
+++ src/main/java/com/example/demo/controllers/rest/ScheduleController.java
@@ -2,14 +2,19 @@ package com.example.demo.controllers.rest;
 
 
 import com.example.demo.config.annotation.Auth;
-import com.example.demo.db.entities.Profile;
-import com.example.demo.db.entities.Schedule;
+import com.example.demo.db.entities.*;
+import com.example.demo.db.repo.AvailableShiftsRepo;
+import com.example.demo.db.repo.ProfileRepo;
 import com.example.demo.db.repo.ScheduleRepo;
+import com.example.demo.db.repo.ShiftRequestRepo;
 import com.google.gson.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.sql.Timestamp;
 import java.util.Collection;
 
 
@@ -23,9 +28,18 @@ public class ScheduleController extends RestApiAbstract<Schedule, ScheduleRepo, 
 
     final Logger logger = LogManager.getLogger(ScheduleController.class);
     final ScheduleRepo repo;
+    final ProfileRepo profileRepo;
+    final AvailableShiftsRepo availableShiftsRepo;
+    final ShiftRequestRepo shiftRequestRepo;
 
-    public ScheduleController(ScheduleRepo repo) {
+
+
+    public ScheduleController(ScheduleRepo repo, ProfileRepo profileRepo, AvailableShiftsRepo availableShiftsRepo, ShiftRequestRepo shiftRequestRepo) {
         this.repo = repo;
+        this.profileRepo = profileRepo;
+        this.availableShiftsRepo = availableShiftsRepo;
+
+        this.shiftRequestRepo = shiftRequestRepo;
     }
 
     @Override
@@ -50,6 +64,46 @@ public class ScheduleController extends RestApiAbstract<Schedule, ScheduleRepo, 
             res.add(object);
         }
         return res.toString();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/addWorker", produces = "application/json")
+    public String addWorker(@RequestBody String request) {
+        int shiftId = 0;
+        int profileId = 0;
+        JsonObject jsonRequest;
+        try {
+            jsonRequest = JsonParser.parseString(request).getAsJsonObject();
+            shiftId = jsonRequest.get("sId").getAsInt();
+            profileId = jsonRequest.get("pId").getAsInt();
+            User user = profileRepo.findById(profileId).get().getUsers().stream().toList().get(0);
+            AvailableShifts shift = availableShiftsRepo.findById(shiftId).get();
+            ShiftsRequests shiftsRequestsequest = new ShiftsRequests();
+            shiftsRequestsequest.setUser(user);
+            shiftsRequestsequest.setShift(shift);
+            shiftsRequestsequest.setTimestamp(new Timestamp(System.currentTimeMillis()));
+            Schedule s = this.createSchedule(shiftsRequestsequest);
+        } catch (Exception e) {
+            logger.error(e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        JsonObject response = new JsonObject();
+        response.addProperty("result", true);
+        return response.toString();
+    }
+
+    public Schedule createSchedule(ShiftsRequests request) {
+        try {
+            Schedule schedule = new Schedule();
+            request = shiftRequestRepo.save(request);
+            schedule.setRequestId(request.getId());
+            schedule.setRequest(request);
+            schedule.setWeekNumber(request.getShift().getWeekNumber());
+            return repo.save(schedule);
+        } catch (Exception e) {
+            logger.error(e);
+            return null;
+        }
     }
 
 }
